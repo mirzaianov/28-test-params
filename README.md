1ccc00f09932d86bf7e0918a8d29ea3e6509d3fe
msejak <7o.mseJak@gmail.com>
2024-04-16 15:08:24 +0300

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# The Params Project
+
+![MasterHead]()
+
+## Description
+
+A test project that renders inputs
+
+### Features
+
+- React components, that allow to update the stucture of Model - to enter values of the parameters.
+- The parameters are completely rendered and are ready to be updated.
+- The full structure with all parameters' values can be received with the getModel() method.
+- The solution is easily expandable (f.e. new parameter types can be added, not only `string`, but also `number`).
+- The provided solution works with `string` in Input parameters out of the box.
+- The solution is prepared in one `main.tsx` file.
+
+### Dependencies
+
+- Vite
+- React + TypeScript + Tailwind
+
+### Installation
+
+A start in the development mode:
+
+    npm create vite@latest
+    cd my-project
+    npm install
+
+### Execution
+
+    npm run dev
+
+Vite will start:
+
+- frontend server on http://localhost:5173/
+
+! The production mode has not been tested.
+
+## Authors
+
+@mirzaianov
+
+## Version History
+
+- 1.0.0 First Release
---
+++ src/main.tsx
@@ -0,0 +1,259 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom/client';
+// eslint-disable-next-line
+import { HiPlusCircle, HiMinusCircle } from 'react-icons/hi';
+import './index.css';
+
+const initialTypes = {
+  string: 'text',
+  number: 'number',
+  boolean: 'checkbox',
+};
+
+interface Param {
+  id: number;
+  name: string;
+  type: 'string' | 'number' | 'boolean';
+}
+
+interface ParamValue {
+  paramId: number;
+  value: string;
+}
+
+interface Model {
+  paramValues: ParamValue[];
+}
+
+interface Props {
+  params: Param[];
+  model: Model;
+  deleteParam: (index: number) => void;
+  // eslint-disable-next-line
+  newParam: (name: string, type: any) => void;
+  changeParam: (index: number, value: string) => void;
+  changeModel: (index: number, value: string) => void;
+}
+
+const initialParams: Param[] = [
+  { id: 1, name: 'Назначение', type: 'string' },
+  { id: 2, name: 'Длина', type: 'string' },
+];
+
+const initialModel: Model = {
+  paramValues: [
+    { paramId: 1, value: 'повседневное' },
+    { paramId: 2, value: 'макси' },
+  ],
+};
+
+// ! Fast refresh only works when a file has exports. Move your component(s) to a separate file.
+// eslint-disable-next-line
+const ParamEditor = (props: Props): JSX.Element => {
+  const getModel = () => {
+    const result = document.getElementById('result')!;
+    result.textContent = JSON.stringify(props.model, undefined, 2);
+  };
+
+  const addNew = (e: React.FormEvent) => {
+    e.preventDefault();
+    const input = document.getElementById('new-param') as HTMLInputElement;
+    const type = document.getElementById('new-param-type') as HTMLSelectElement;
+    props.newParam(input.value, type.value);
+    input.value = '';
+  };
+
+  return (
+    <div className={`params__editor flex flex-col gap-10`}>
+      {/* ! Show current Params */}
+      <form className={`params__choice flex flex-col gap-2`}>
+        <h2 className={`self-center text-xl`}>Edit Current Params</h2>
+        {props.params.map((param) => (
+          <div
+            className={`input-control flex gap-2`}
+            key={param.id}
+          >
+            <input
+              className={`input input-bordered w-full max-w-xs`}
+              type="text"
+              name={param.id.toString()}
+              id={param.id.toString()}
+              value={param.name}
+              onChange={(e) => props.changeParam(param.id, e.target.value)}
+            />
+            <select
+              className={`select select-bordered w-full max-w-xs`}
+              name="type"
+              id={param.name.toString()}
+              defaultValue={param.type}
+            >
+              {Object.entries(initialTypes).map((value) => (
+                <option
+                  key={`${param.id}${value[0]}`}
+                  value={value[1]}
+                >
+                  {value[0]}
+                </option>
+              ))}
+            </select>
+            <button
+              className="btn btn-outline btn-error p-3"
+              type="button"
+              onClick={() => props.deleteParam(param.id)}
+            >
+              <HiMinusCircle className={`text-error h-6 w-6`} />
+            </button>
+          </div>
+        ))}
+      </form>
+      {/* ! Add a new Param */}
+      <form
+        className={`params__add flex flex-col gap-2`}
+        onSubmit={addNew}
+      >
+        <h2 className={`self-center text-xl`}>Add a new Param</h2>
+        <div className={`flex gap-2`}>
+          <input
+            placeholder={`Type a name`}
+            className={`input input-bordered w-full max-w-xs`}
+            type="text"
+            id="new-param"
+          />
+          <select
+            className={`select select-bordered w-full max-w-xs `}
+            name="type"
+            id="new-param-type"
+            defaultValue={`default`}
+          >
+            <option
+              disabled
+              value="default"
+            >
+              Choose a type
+            </option>
+            {Object.entries(initialTypes).map((value, index) => (
+              <option
+                key={`add-${index}`}
+                value={value[1]}
+              >
+                {value[0]}
+              </option>
+            ))}
+          </select>
+          <button className="btn btn-outline btn-primary p-3">
+            <HiPlusCircle className={`text-primary h-6 w-6`} />
+          </button>
+        </div>
+      </form>
+      {/* ! Show the current Model */}
+      <div className={`params__list flex flex-col gap-2`}>
+        <h2 className={`block self-center text-xl`}>
+          Edit Current Model Values
+        </h2>
+        <ul className={`flex flex-col gap-2`}>
+          {props.params.map((param) => (
+            <li
+              key={param.id}
+              className={`flex gap-2`}
+            >
+              <label
+                htmlFor={`params__list-${param.id}`}
+                className={`form-control flex w-full max-w-xs basis-4/12`}
+              >
+                <div className={`label flex-1`}>
+                  <span className={`label-text text-base`}>{param.name}</span>
+                </div>
+              </label>
+              <input
+                className={`input input-bordered w-full max-w-xs basis-8/12`}
+                id={`params__list-${param.id}`}
+                type={param.type}
+                value={
+                  props.model.paramValues.find(
+                    (item) => item.paramId === param.id,
+                  )?.value
+                }
+                onChange={(e) =>
+                  props.changeModel(param.id, e.target.value || '')
+                }
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
+      <button
+        className="btn btn-secondary"
+        type="button"
+        onClick={getModel}
+      >
+        Show Current Model as JSON
+      </button>
+      <pre id="result"></pre>
+    </div>
+  );
+};
+
+// ! Fast refresh only works when a file has exports. Move your component(s) to a separate file.
+// eslint-disable-next-line
+const App = (): JSX.Element => {
+  const [params, setParams] = useState<Param[]>(initialParams);
+  const [model, setModel] = useState<Model>(initialModel);
+
+  const deleteParam = (index: number) => {
+    setParams((previous) => previous.filter((item) => item.id !== index));
+    setModel({
+      ...model,
+      paramValues: model.paramValues.filter((item) => item.paramId !== index),
+    });
+  };
+
+  // eslint-disable-next-line
+  const newParam = (name: string, type: any) => {
+    const len = params.length;
+    // eslint-disable-next-line
+    setParams((previous) => [...previous, { id: len + 1, name, type }]);
+    setModel({
+      ...model,
+      paramValues: [...model.paramValues, { paramId: len + 1, value: '' }],
+    });
+  };
+
+  const changeParam = (index: number, value: string) => {
+    setParams((previous) =>
+      previous.map((param) =>
+        param.id === index ? { ...param, name: value } : param,
+      ),
+    );
+  };
+
+  const changeModel = (index: number, value: string) => {
+    setModel({
+      ...model,
+      paramValues: model.paramValues.map((item) =>
+        item.paramId === index ? { ...item, value } : item,
+      ),
+    });
+  };
+
+  return (
+    <div
+      className={`app flex w-full flex-col items-center justify-center gap-10 p-4 pt-10`}
+    >
+      <h1 className={`text-5xl`}>Test Task Params</h1>
+      <ParamEditor
+        model={model}
+        params={params}
+        deleteParam={deleteParam}
+        newParam={newParam}
+        changeParam={changeParam}
+        changeModel={changeModel}
+      />
+    </div>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
